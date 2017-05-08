5ced3727340734e7b01712d7cc95bffa8e095e2d
MaRaS <hhjh@seznam.cz>
2017-05-08 10:09:26 +0200

    minor update

--- isbackend/positions/models.py
+++ isbackend/positions/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Position(models.Model):
-    mentor_name = models.CharField(max_length=50)
-    division = models.CharField(max_length=100)
-    pros_of_mentor = models.CharField(max_length=2000)
-    position_name = models.CharField(max_length=50)
-    position_description = models.CharField(max_length=2000)
-    project = models.CharField(max_length=1000)
-    benefit_for_intern = models.CharField(max_length=1000)
-    requirements = models.CharField(max_length=1000)
-    notes = models.CharField(max_length=1000)
-    registration_date = models.DateField(auto_now_add=True)
+    mentor_name = models.CharField('Mentor', max_length=50)
+    division = models.CharField('Divize', max_length=100)
+    pros_of_mentor = models.CharField('Mentorovy přednosti', max_length=2000)
+    position_name = models.CharField('Jméno pozice', max_length=50)
+    position_description = models.CharField('Popis pozice', max_length=2000)
+    project = models.CharField('Projekt', max_length=1000)
+    benefit_for_intern = models.CharField('Výhody pro stážistu', max_length=1000)
+    requirements = models.CharField('Požadavky', max_length=1000)
+    notes = models.CharField('Poznámky', max_length=1000)
+    registration_date = models.DateField('Datum registrace',  auto_now_add=True)
