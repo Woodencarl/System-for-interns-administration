b7af547ce2f3ac2dc0a859da8da02feb33e39d2d
MaRaS <hhjh@seznam.cz>
2017-05-15 05:07:51 +0200

    minor cosmetic fix

--- isbackend/interns/urls.py
+++ isbackend/interns/urls.py
@@ -12,10 +12,10 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^formular/', RegisterView.as_view(), name='internsForm'),
-    url(r'^profil/(?P<intern_id>[0-9a-f-]+)/create_comment/', login_required(create_comment), name="create comment"),
-    url(r'^profil/(?P<intern_id>[0-9a-f-]+)/uzavrit/', login_required(close_profile), name="close profile"),
     url(r'^smazat/(?P<intern_id>[0-9a-f-]+)/$', erase_profile, name="smazat"),
     url(r'^poslat/(?P<intern_id>[0-9a-f-]+)', send_to_intern, name="poslat"),
+    url(r'^profil/(?P<intern_id>[0-9a-f-]+)/create_comment/', login_required(create_comment), name="create comment"),
+    url(r'^profil/(?P<intern_id>[0-9a-f-]+)/uzavrit/', login_required(close_profile), name="close profile"),
     url(r'^profil/(?P<intern_id>[0-9a-f-]+)/editovat_profil/save/', login_required(save_edit_profile), name="save edit profile"),
     url(r'^profil/(?P<intern_id>[0-9a-f-]+)/editovat_profil/', login_required(edit_profile), name="editovat profil"),
     url(r'^profil/(?P<intern_id>[0-9a-f-]+)/$', login_required(ProfileView.as_view()), name="profil"),
